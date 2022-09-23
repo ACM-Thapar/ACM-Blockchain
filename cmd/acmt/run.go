677807214febb5a249b53512f1565d8052b3b1c5
Amit <agoyal3_be21@thapar.edu>
2022-09-23 14:06:59 +0530

    minor fix

--- cmd/acmt/run.go
+++ cmd/acmt/run.go
@@ -11,11 +11,11 @@ import (
 func runCmd() *cobra.Command {
 	var runCmd = &cobra.Command{
 		Use:   "run",
-		Short: "Launches the TBB node and its HTTP API.",
+		Short: "Launches the ACMT node and its HTTP API.",
 		Run: func(cmd *cobra.Command, args []string) {
 			dataDir, _ := cmd.Flags().GetString(flagDataDir)
 
-			fmt.Println("Launching TBB node and its HTTP API...")
+			fmt.Println("Launching ACM TIET's Blockchain node and its HTTP API...")
 
 			err := node.Run(dataDir)
 			if err != nil {
